861b4909da9f0c0081d87137ee7478513ae4cddb
pneu <pneumaster@gmail.com>
2016-03-12 02:05:35 +0900

    fixup

--- SleepMe/SleepMe/Program.cs
+++ SleepMe/SleepMe/Program.cs
@@ -11,24 +11,24 @@ namespace SleepWindows
 {
     static class Program
     {
-        static ManualResetEvent _resetEvent = new ManualResetEvent(true);
+        static ManualResetEvent _cancelEvent = new ManualResetEvent(false);
         static System.Timers.Timer timer = new System.Timers.Timer();
         static void Main(string[] args) {
             bracket(() => {
-                var time = double.Parse(args[0]);
-                Console.WriteLine($"specified {time} seconds.");
-                var now = DateTime.Now;
-                PrintSleepTime(now, time);
-                bracket(() => Go(Console.ReadKey().Key),
+                var remain = GetSecond(args[0]);
+                Console.WriteLine($"specified {remain} seconds.");
+                PrintSleepTime(DateTime.Now, remain);
+                bracket(() => IsGo(Console.ReadKey().Key),
                     () => {
                         Console.WriteLine("\nStart.");
                         var sleepTask = Task.Run(() => {
-                            GoToBed(time);
+                            GoToBed(remain);
+                            _cancelEvent.WaitOne();
                         });
-                        while (!Cancel(Console.ReadKey())) { }
+                        while (!IsCancel(Console.ReadKey().Key)) { }
+                        _cancelEvent.Set();
+                        sleepTask.Wait();
                         Console.WriteLine("Cancelled by user. quit.");
-                        _resetEvent.Reset();
-                        sleepTask.Dispose();
                     }, elseAction: () => {
                         Console.WriteLine("\nuser cancelled. quit.");
                         Wait();
@@ -39,32 +39,51 @@ namespace SleepWindows
             });
         }
 
-        private static void PrintSleepTime(DateTime now, double time) {
-            var sleepTime = DateTime.Now.AddSeconds(time);
+        private static double GetSecond(string arg) {
+            if (arg.EndsWith("m")) {
+                return double.Parse(arg.Substring(0, arg.Length - "m".Length)) * 60;
+            }
+            else if (arg.EndsWith("min")) {
+                return double.Parse(arg.Substring(0, arg.Length - "min".Length)) * 60;
+            }
+            else if (arg.EndsWith("h")) {
+                return double.Parse(arg.Substring(0, arg.Length - "h".Length)) * 3600;
+            }
+            else if (arg.EndsWith("hour")) {
+                return double.Parse(arg.Substring(0, arg.Length - "hour".Length)) * 3600;
+            }
+            else {
+                return double.Parse(arg);
+            }
+        }
+
+        private static void PrintSleepTime(DateTime now, double remain) {
+            var sleepTime = DateTime.Now.AddSeconds(remain);
             var diffTime = sleepTime - now;
-            Console.Write($"Sleep Me at {sleepTime} (about {(diffTime.TotalSeconds / 3600.0):F1} hours). OK? [Y/else]");
+            Console.Write($"Sleep Me at {sleepTime} (about {(diffTime.TotalSeconds / 3600.0):F1} hours). OK? [Y/else] ");
         }
 
         private static void Wait() {
             Console.ReadKey();
         }
 
-        private static bool Go(ConsoleKey key) {
+        private static bool IsGo(ConsoleKey key) {
             return key == ConsoleKey.Y;
         }
 
-        private static bool Cancel(ConsoleKeyInfo key) {
-            return key.Modifiers == ConsoleModifiers.Control && key.Key == ConsoleKey.C;
+        private static bool IsCancel(ConsoleKey key) {
+            return key == ConsoleKey.Q;
         }
 
         private static void GoToBed(double elapsed) {
+            timer.AutoReset = false;
             timer.Elapsed += Timer_Elapsed;
-            timer.Interval = elapsed;
+            timer.Interval = 1000 * elapsed;
             timer.Start();
         }
         private static void Timer_Elapsed(object _, ElapsedEventArgs __) {
-            _resetEvent.WaitOne();
-            Application.SetSuspendState(PowerState.Suspend, false, true);
+            if (!_cancelEvent.WaitOne(0))
+                Application.SetSuspendState(PowerState.Suspend, false, true);
         }
 
         private static void bracket(Action action, Action onException) {
